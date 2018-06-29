298323bb13c576613ae767d0cd93b4b182d3c3b2
Stephen Rooney <s.f.rooney@gmail.com>
2018-06-29 13:48:56 +0100

    rooms are independantly populated

--- src/test/java/Rooms/subRooms/ConferenceRoomTest.java
+++ src/test/java/Rooms/subRooms/ConferenceRoomTest.java
@@ -1,5 +1,6 @@
 package Rooms.subRooms;
 
+import Rooms.Guest;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,31 +8,46 @@ import static org.junit.Assert.*;
 
 public class ConferenceRoomTest {
 
+    ConferenceRoom conferenceRoom;
+    Guest guest;
+
     @Before
-    public void setUp() throws Exception {
+    public void before() {
+        conferenceRoom = new ConferenceRoom("Function room", 100, 100);
+        guest = new Guest("Martin");
     }
 
     @Test
     public void getRate() {
+        assertEquals(100, conferenceRoom.getRate());
     }
 
     @Test
     public void getCapacity() {
+        assertEquals(100, conferenceRoom.getCapacity());
     }
 
     @Test
     public void getGuestnumber() {
+        assertEquals(0, conferenceRoom.getGuestnumber());
     }
 
     @Test
     public void getName() {
+        assertEquals("Function room", conferenceRoom.getName());
     }
 
     @Test
     public void checkIn() {
+        conferenceRoom.checkIn(guest);
+        assertEquals(1, conferenceRoom.getGuestnumber());
+
     }
 
     @Test
     public void checkOut() {
+        conferenceRoom.checkIn(guest);
+        conferenceRoom.checkOut(guest);
+        assertEquals(0, conferenceRoom.getGuestnumber());
     }
 }
